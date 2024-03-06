2594ca5243d89edf777bc8cf78cab6886915aa84
BijonTalukder <bijontalukder1247@gmail.com>
2024-03-06 13:41:37 +0600

    authenticate done

--- app/(routes)/dashboard/page.tsx
+++ app/(routes)/dashboard/page.tsx
@@ -1,9 +1,13 @@
+"use client"
+import { Button } from '@/components/ui/button'
+import { LoginLink } from '@kinde-oss/kinde-auth-nextjs'
 import React from 'react'
 
 export default function dashboard() {
   return (
     <div>
       dashboard
+      <Button><LoginLink>Logout</LoginLink></Button>
     </div>
   )
 }
