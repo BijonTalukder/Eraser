8cb0a40648e1004837f111e5c0c2873fb37867e5
BijonTalukder <bijontalukder1247@gmail.com>
2024-03-07 17:15:33 +0600

    convex alternative query done

--- app/(routes)/dashboard/page.tsx
+++ app/(routes)/dashboard/page.tsx
@@ -2,11 +2,12 @@
 import { Button } from '@/components/ui/button'
 import { api } from "@/convex/_generated/api";
 import { LoginLink, useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
-import { useMutation, useQueries, useQuery } from "convex/react";
+import { useConvex, useMutation, useQueries, useQuery } from "convex/react";
 import React, { useEffect } from "react";
 
 export default function dashboard() {
   const { user }: any = useKindeBrowserClient();
+  const convex = useConvex();
   const getUser = useQuery(api.user.getUser, { email: user?.email });
 
   const createUser = useMutation(api.user.createUser);
@@ -25,6 +26,10 @@ export default function dashboard() {
       }
     }
   }, []);
+
+  const checkUsr = async () => {
+    const result = await convex.query(api.user.getUser,{email:user?.email});
+  };
   return (
     <div>
       dashboard
