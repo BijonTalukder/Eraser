18e3ca07ae86430bd62ba1081c28a7857dd9e1c5
BijonTalukder <bijontalukder1247@gmail.com>
2024-03-07 17:07:17 +0600

    convex query done

--- app/(routes)/dashboard/page.tsx
+++ app/(routes)/dashboard/page.tsx
@@ -1,13 +1,36 @@
 "use client"
 import { Button } from '@/components/ui/button'
-import { LoginLink } from '@kinde-oss/kinde-auth-nextjs'
-import React from 'react'
+import { api } from "@/convex/_generated/api";
+import { LoginLink, useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
+import { useMutation, useQueries, useQuery } from "convex/react";
+import React, { useEffect } from "react";
 
 export default function dashboard() {
+  const { user }: any = useKindeBrowserClient();
+  const getUser = useQuery(api.user.getUser, { email: user?.email });
+
+  const createUser = useMutation(api.user.createUser);
+  useEffect(() => {
+    if (user) {
+      console.log(getUser);
+
+      if (getUser == undefined) {
+        createUser({
+          name: user.given_name,
+          email: user.email,
+          image: user.picture,
+        }).then((resp) => {
+          console.log(resp);
+        });
+      }
+    }
+  }, []);
   return (
     <div>
       dashboard
-      <Button><LoginLink>Logout</LoginLink></Button>
+      <Button>
+        <LoginLink>Logout</LoginLink>
+      </Button>
     </div>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,8 @@ import { useEffect } from "react";
 
 export default function Home() {
   const { user } = useKindeBrowserClient();
+
+  
   useEffect(() => {
     console.log(user);
   }, [user]);
